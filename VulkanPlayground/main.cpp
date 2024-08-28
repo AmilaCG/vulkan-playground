c6a4320bc3edbddad01b9c490c868813cadfb2d6
Amila Abeygunasekara <auabey@gmail.com>
2024-08-28 03:16:40 -0400

    Reset project for Vulkan Guide

--- VulkanPlayground/main.cpp
+++ VulkanPlayground/main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 
-#include "Renderer/RenderBackend.h"
-
 int main()
 {
-    try
-    {
-        RenderBackend renderBackend;
-        renderBackend.Init();
-        renderBackend.RunRenderLoop();
-        renderBackend.Shutdown();
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-
+    std::cout << "Hello!" << std::endl;
     return EXIT_SUCCESS;
 }
