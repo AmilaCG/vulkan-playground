c49fc9930b178078c36444b2307fa8e9819e25ba
Amila Abeygunasekara <auabey@gmail.com>
2024-09-27 00:32:02 -0400

    Start implementing GLTF loader
    Note: <fastgltf/parser.hpp> is not supported anymore. Using latest fastgltf APIs in this commit.

--- VulkanPlayground/src/vk_loader.cpp
+++ VulkanPlayground/src/vk_loader.cpp
@@ -1,1 +1,39 @@
 ﻿#include "vk_loader.h"
+
+#include <iostream>
+#include <stb_image.h>
+#include <glm/gtx/quaternion.hpp>
+#include <fastgltf/glm_element_traits.hpp>
+#include <fastgltf/core.hpp>
+#include <fastgltf/tools.hpp>
+
+#include "vk_loader.h"
+#include "vk_engine.h"
+#include "vk_initializers.h"
+#include "vk_types.h"
+
+std::optional<std::vector<std::shared_ptr<MeshAsset>>> load_gltf_meshes(VulkanEngine* engine,
+                                                                        std::filesystem::path filePath)
+{
+    std::cout << "Loading GLTF: " << filePath << std::endl;
+
+    fastgltf::GltfDataBuffer data;
+    data.loadFromFile(filePath);
+
+    constexpr auto gltfOptions = fastgltf::Options::LoadGLBBuffers |
+                                 fastgltf::Options::LoadExternalBuffers;
+
+    fastgltf::Asset gltf;
+    fastgltf::Parser parser{};
+
+    auto load = parser.loadGltf(&data, filePath.parent_path(), gltfOptions);
+    if (load)
+    {
+        gltf = std::move(load.get());
+    }
+    else
+    {
+        fmt::print("Failed to load glTF: {} \n", fastgltf::to_underlying(load.error()));
+        return {};
+    }
+}
--- VulkanPlayground/src/vk_loader.h
+++ VulkanPlayground/src/vk_loader.h
@@ -1,1 +1,25 @@
 ﻿#pragma once
+
+#include <unordered_map>
+#include <filesystem>
+
+#include "vk_types.h"
+
+struct GeoSurface
+{
+    uint32_t startIndex;
+    uint32_t count;
+};
+
+struct MeshAsset
+{
+    std::string name;
+
+    std::vector<GeoSurface> surfaces;
+    GPUMeshBuffers meshBuffers;
+};
+
+class VulkanEngine;
+
+std::optional<std::vector<std::shared_ptr<MeshAsset>>> load_gltf_meshes(VulkanEngine* engine,
+                                                                        std::filesystem::path filePath);
