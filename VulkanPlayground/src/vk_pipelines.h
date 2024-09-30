eed8f80531350107356bb967e107732df9ae35f3
Amila Abeygunasekara <auabey@gmail.com>
2024-09-30 01:04:21 -0400

    Enable blending and make the monkey head semi-transparent

--- VulkanPlayground/src/vk_pipelines.h
+++ VulkanPlayground/src/vk_pipelines.h
@@ -22,6 +22,8 @@ public:
     void set_depth_format(VkFormat format);
     void disable_depthtest();
     void enable_depthtest(bool depthWriteEnable, VkCompareOp op);
+    void enable_blending_additive();
+    void enable_blending_alphablend();
 
     std::vector<VkPipelineShaderStageCreateInfo> _shaderStages;
 
